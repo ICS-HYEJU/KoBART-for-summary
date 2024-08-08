57d18e391bd4d4e0f31631c58db8df8d20e716fa
hjchoi <juhye987@naver.com>
2024-08-08 19:21:53 +0900

    [MODIFY] split.py

--- data/split.py
+++ data/split.py
@@ -1,13 +1,14 @@
 import jsonlines
 import json
 import time
+import re
 import pandas as pd
 import itertools
 
 
 from tqdm import tqdm
 from ast import literal_eval
-
+from transformers import PreTrainedTokenizerFast
 import sentencepiece as spm
 
 def split_file():
@@ -20,31 +21,24 @@ def split_file():
             with open(make_path + str(i) + '.json', 'w', encoding='UTF-8') as f:
                 f.write(json.dumps(doc[i], ensure_ascii=False))
 
-
 # We can use this function as a getitem function.
 # Remove Stopwords
 def read_file():
+    from config.config import get_config_dict
+    cfg = get_config_dict()
+    tokenizer = PreTrainedTokenizerFast.from_pretrained(cfg.dataset_info['pretrained_name'])
     path = '/storage/hjchoi/Document_Summary_text/Training/news_train_split/0.json'
     with open(path, 'r') as f:
         data = json.load(f)
         print(data)
         text = list(itertools.chain(*data['text']))
-        #
-        # extract stopwords
-        # stopword=[]
-        # for id in tqdm(range(len(text)), desc="extracting stopwords",mininterval=0.01):
-        #     for i,v in enumerate(text[id]['sentence']):
-        #         if i in list(literal_eval(text[3]['highlight_indices'])):
-        #             stopword.append(v)
-        #     time.sleep(0.1)
-        # print(stopword)
-        #
         # remove stopwords
         sentence = []
         line = ''
         for id in tqdm(range(len(text)), desc="remove stopwords from origin", mininterval=0.01):
+            stop_idx = re.split(r'[,;]', text[id]['highlight_indices'])
             for i, v in enumerate(text[id]['sentence']):
-                if i in list(literal_eval(text[3]['highlight_indices'])):
+                if str(i) in stop_idx:
                     continue
                 line += v
             sentence.append(line)
@@ -52,7 +46,8 @@ def read_file():
             time.sleep(0.1)
         print(sentence)
 
+
 if __name__ == '__main__':
-    #split_file()
+    # split_file()
     read_file()
 
