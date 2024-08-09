a98ec3bef0d237f0804f19c30b501fcb870959fb
hjchoi <juhye987@naver.com>
2024-08-09 22:51:29 +0900

    [ORIGINAL] split.py

--- data/split.py
+++ data/split.py
@@ -12,8 +12,8 @@ from transformers import PreTrainedTokenizerFast
 import sentencepiece as spm
 
 def split_file():
-    path = '/storage/hjchoi/Document_Summary_text/Training/news_train_original/train_original.json'
-    make_path = '/storage/hjchoi/Document_Summary_text/Training/news_train_split/'
+    path = '/storage/hjchoi/Document_Summary_text/Training/law_train_original/train_original.json'
+    make_path = '/storage/hjchoi/Document_Summary_text/Training/law_train_split/'
     with open(path, 'r') as f:
         json_file = json.load(f)
         doc = json_file['documents']
@@ -27,25 +27,32 @@ def read_file():
     from config.config import get_config_dict
     cfg = get_config_dict()
     tokenizer = PreTrainedTokenizerFast.from_pretrained(cfg.dataset_info['pretrained_name'])
-    path = '/storage/hjchoi/Document_Summary_text/Training/news_train_split/0.json'
-    with open(path, 'r') as f:
-        data = json.load(f)
-        print(data)
-        text = list(itertools.chain(*data['text']))
-        # remove stopwords
-        sentence = []
-        line = ''
-        for id in tqdm(range(len(text)), desc="remove stopwords from origin", mininterval=0.01):
-            stop_idx = re.split(r'[,;]', text[id]['highlight_indices'])
-            for i, v in enumerate(text[id]['sentence']):
-                if str(i) in stop_idx:
-                    continue
-                line += v
-            sentence.append(line)
+    #
+    for jf in tqdm(range(243983), desc='Remake data[text]', mininterval=0.01):
+        path = '/storage/hjchoi/Document_Summary_text/Training/news_train_split/'
+        with open(path+str(jf)+'.json', 'r') as f:
+            data = json.load(f)
+            text = list(itertools.chain(*data['text']))
+            text_line = []
+            make_data = []
+            # remove stopwords
             line = ''
+            for id in tqdm(range(len(text)), desc="remove stopwords from origin", mininterval=0.01):
+                stop_idx = re.split(r'[,;]', text[id]['highlight_indices'])
+                for i, v in enumerate(text[id]['sentence']):
+                    if str(i) in stop_idx:
+                        continue
+                    line += v
+                text[id]['sentence'] = [tokenizer.bos_token_id] + tokenizer.encode(line) + [tokenizer.eos_token_id]
+                del text[id]['highlight_indices']
+                text[id]['summary'] = tokenizer.encode(data['abstractive'][0])
+                make_data.append(text[id])
+                line = ''
+            with open('/storage/hjchoi/Document_Summary_text/Training/news/' + str(jf)+'.json', 'w', encoding='UTF-8') as f:
+                f.write(json.dumps(make_data, ensure_ascii=False))
+            make_data = []
             time.sleep(0.1)
-        print(sentence)
-
+        time.sleep(0.1)
 
 if __name__ == '__main__':
     # split_file()
