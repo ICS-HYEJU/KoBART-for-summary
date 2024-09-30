7c732744ff87fc653701dbf0d1928ee3a13be022
hjchoi <juhye987@naver.com>
2024-09-30 21:24:13 +0900

    [ADD] concat.py

---
+++ data/concat.py
@@ -0,0 +1,25 @@
+import pandas as pd
+import glob
+import os
+
+def concat_file():
+    input_f = '/storage/hjchoi/Document_Summary_text/Training/'
+    output_f = '/storage/hjchoi/Document_Summary_text/Training/nm.tsv'
+
+    allFiles = glob.glob(input_f + '*.tsv')
+    print(allFiles)
+
+    allData = []
+    for file in allFiles:
+        if file.split('/')[-1] =='magazine.tsv':
+            continue
+        df = pd.read_csv(file, sep='\t')
+        allData.append(df)
+
+    dataconcat = pd.concat(allData, ignore_index=True)
+    dataconcat.drop('id',axis=1,inplace=True)
+    dataconcat.dropna(inplace=True)
+    dataconcat.to_csv(output_f,sep='\t', index=False)
+
+if __name__ == '__main__':
+    concat_file()
